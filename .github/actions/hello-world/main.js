d6bac6240c9884c914e49f53cf7f10abd6d96c1f
Chedda98 <twa05157@gmail.com>
2021-09-13 16:11:06 +0900

    allow input in all action files

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const core = require("@actions/core");
+
+const firstGreeting = core.getInput("first-greeting"); //core.getInput()을 사용하면 action.yml 내부에 배치한 모든 입력 매개변수의 문자열을 지정할 수 있다!
+const secondGreeting = core.getInput("second-greeting");
+const thirdGreeting = core.getInput("third-greeting");
+
+console.log(`Hello ${firstGreeting}`);
+console.log(`Hello ${secondGreeting}`);
+if (thirdGreeting) {
+    console.log(`Hello ${thirdGreeting}`); //콘솔에 다양한 방법으로 결과를 찍어낼 수 있다!
+}
